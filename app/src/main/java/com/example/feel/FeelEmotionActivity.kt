e619cb707e2ceb524f0dfe19d9fad0bd036d8874
Hain Zuppur <h.zuppur@gmail.com>
2020-10-12 20:18:55 +0300

    #17 Added feeling time activity

--- app/src/main/java/com/example/feel/FeelEmotionActivity.kt
+++ app/src/main/java/com/example/feel/FeelEmotionActivity.kt
@@ -1,7 +1,9 @@
 package com.example.feel
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -11,5 +13,11 @@ class FeelEmotionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.feeling_emotion)
+
+        val nextButton = findViewById<Button>(R.id.NextButton)
+        nextButton.setOnClickListener {
+            val intent = Intent(this, FeelWhenActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/feel/FeelWhenActivity.kt
@@ -0,0 +1,49 @@
+package com.example.feel
+
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.util.*
+
+
+class FeelWhenActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n", "SimpleDateFormat")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.feeling_when)
+
+        val mPickTimeBtn = findViewById<Button>(R.id.pickDateBtn)
+        val mPickTimeToday = findViewById<Button>(R.id.buttonToday)
+        val textView = findViewById<TextView>(R.id.dateTv)
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        mPickTimeBtn.setOnClickListener {
+            val dpd = DatePickerDialog(
+                this,
+                { view, year, monthOfYear, dayOfMonth ->
+                    // Display Selected date in TextView
+                    textView.text = "$dayOfMonth/ $month/ $year"
+                }, year, month, day
+            )
+            dpd.show()
+        }
+        mPickTimeToday.setOnClickListener {
+            val sdf = SimpleDateFormat("dd/ M/ yyyy")
+            val currentDate = sdf.format(Date())
+            // Display Selected date in TextView
+            textView.text = currentDate
+        }
+    }
+}
