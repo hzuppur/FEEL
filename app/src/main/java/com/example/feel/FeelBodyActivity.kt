e8f62eb46ac2f8805461af03fb7047a8f2cf0af8
Hain Zuppur <h.zuppur@gmail.com>
2020-10-12 19:22:09 +0300

    #2 Added what triggerd you activity

--- app/src/main/java/com/example/feel/FeelBodyActivity.kt
+++ app/src/main/java/com/example/feel/FeelBodyActivity.kt
@@ -1,9 +1,8 @@
 package com.example.feel
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -11,5 +10,11 @@ class FeelBodyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.feeling_body)
+
+        val nextButton = findViewById<Button>(R.id.NextButton)
+        nextButton.setOnClickListener {
+            val intent = Intent(this, FeelTriggerActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/feel/FeelTriggerActivity.kt
@@ -0,0 +1,15 @@
+package com.example.feel
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class FeelTriggerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.feeling_trigger)
+    }
+}
