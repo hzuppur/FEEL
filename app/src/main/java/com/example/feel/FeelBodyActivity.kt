e84c30c4f16b9f586480c25f07ef1f95c1ac66d4
Hain Zuppur <h.zuppur@gmail.com>
2020-10-12 19:04:07 +0300

    #3 added body image

--- app/src/main/java/com/example/feel/FeelBodyActivity.kt
+++ app/src/main/java/com/example/feel/FeelBodyActivity.kt
@@ -8,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class FeelBodyActivity : AppCompatActivity() {
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.feeling_body)
